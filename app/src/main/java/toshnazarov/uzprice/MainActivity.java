0a3e74de2d6c7d9cab728145d1e09ff06fdf4161
Qobiljon <qobiljon.toshnazarov@gmail.com>
2016-11-26 17:00:15 -0800

    Design, first changes

--- app/src/main/java/toshnazarov/uzprice/MainActivity.java
+++ app/src/main/java/toshnazarov/uzprice/MainActivity.java
@@ -2,6 +2,9 @@ package toshnazarov.uzprice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialize();
+    }
+
+    private void initialize() {
+        View icon = findViewById(R.id.app_icon);
+
+        Animation icon_anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.icon_anim);
+        icon_anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                View title = findViewById(R.id.app_title);
+                View quot = findViewById(R.id.app_quot);
+                title.setVisibility(View.VISIBLE);
+                quot.setVisibility(View.VISIBLE);
+
+                Animation title_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.title_anim);
+                Animation quot_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.quot_anim);
+
+                title.startAnimation(title_in);
+                quot.startAnimation(quot_in);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        icon.startAnimation(icon_anim);
     }
 }
