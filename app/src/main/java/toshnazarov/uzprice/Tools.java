9db84e1ec27e03a7ff866313f8aea88670f17404
Qobiljon <qobiljon.toshnazarov@gmail.com>
2016-11-27 11:05:00 -0800

    temp commit

---
+++ app/src/main/java/toshnazarov/uzprice/Tools.java
@@ -0,0 +1,45 @@
+package toshnazarov.uzprice;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+@SuppressWarnings("unused")
+public class Tools {
+    public static byte[] download(String link) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        InputStream is = null;
+        try {
+            is = new URL(link).openStream();
+            byte[] byteChunk = new byte[4096];
+            int n;
+            while ((n = is.read(byteChunk)) > 0)
+                baos.write(byteChunk, 0, n);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        byte[] res = baos.toByteArray();
+        try {
+            baos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    public static String downloadString(String link) {
+        byte[] raw = download(link);
+        return new String(raw, Charset.forName("utf-8"));
+    }
+}
